2f316de5d7e26218779a6cf186472b9bab30698c
hokkyar <hokkyaryasta@gmail.com>
2023-12-11 09:38:22 +0700

    editing admin dashboard display

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -13,8 +13,8 @@
                 <i class="bi bi-disc-fill text-white"></i>
             </h5>
         </div>
-        <div class="p-3 w-75 text-center">
-            <h5 class="card-title">Nama STT Anda : {{ $nama_stt }}</h5>
+        <div class="p-3 w-75">
+            <h5 class="card-title mx-2">Nama STT : {{ $nama_stt }}</h5>
         </div>
     </div>
 
@@ -24,19 +24,8 @@
                 <i class="bi bi-person-lines-fill text-white"></i>
             </h5>
         </div>
-        <div class="p-3 w-75 text-center">
-            <h5 class="card-title">Jumlah Anggota : {{ $jumlah_anggota }}</h5>
-        </div>
-    </div>
-
-    <div class="d-flex border border-danger rounded w-75 mx-auto align-items-center mt-3">
-        <div class="p-3 w-25 text-center bg-danger">
-            <h5 class="card-title">
-                <i class="bi bi-newspaper text-white"></i>
-            </h5>
-        </div>
-        <div class="p-3 w-75 text-center">
-            <h5 class="card-title">Berita Terkini : {{ $berita_terkini->judul }}</h5>
+        <div class="p-3 w-75">
+            <h5 class="card-title mx-2">Jumlah Anggota : {{ $jumlah_anggota }} orang</h5>
         </div>
     </div>
 
@@ -46,8 +35,21 @@
                 <i class="bi bi-activity text-white"></i>
             </h5>
         </div>
-        <div class="p-3 w-75 text-center">
-            <h5 class="card-title">Kegiatan Terbaru : {{ $kegiatan_terbaru->nama_kegiatan }}</h5>
+        <div class="p-3 w-75">
+            <h5 class="card-title mx-2">Kegiatan Terbaru : <a href="/admin/kegiatan/view/{{ $kegiatan_terbaru->id }}"
+                    style="text-decoration: none;">{{ $kegiatan_terbaru->nama_kegiatan }}</a></h5>
+        </div>
+    </div>
+
+    <div class="d-flex border border-danger rounded w-75 mx-auto align-items-center mt-3">
+        <div class="p-3 w-25 text-center bg-danger">
+            <h5 class="card-title">
+                <i class="bi bi-newspaper text-white"></i>
+            </h5>
+        </div>
+        <div class="p-3 w-75">
+            <h5 class="card-title mx-2">Berita Terkini : <a href="/admin/berita/view/{{ $berita_terkini->id }}"
+                    style="text-decoration: none;">{{ $berita_terkini->judul }}</a></h5>
         </div>
     </div>
 @endsection
