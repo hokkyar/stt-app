b78a2c9d1b57631678c7cbbc469e5cb8ffb759e2
hokkyar <hokkyaryasta@gmail.com>
2023-12-11 09:54:24 +0700

    editing kegiatan card to table view

--- resources/views/pages/admin/kegiatan/index.blade.php
+++ resources/views/pages/admin/kegiatan/index.blade.php
@@ -20,7 +20,7 @@
         <input type="text" class="form-control">
     </div>
 
-    <div class="d-flex flex-wrap gap-3 justify-content-center my-3">
+    {{-- <div class="d-flex flex-wrap gap-3 justify-content-center my-3">
         @foreach ($all_kegiatan as $kegiatan)
             <div class="card" style="width: 20rem;">
                 <img src="{{ asset('img/background.jpg') }}" class="card-img-top" alt="picture">
@@ -32,5 +32,43 @@
                 </div>
             </div>
         @endforeach
+    </div> --}}
+
+    <div class="w-100 card mb-3 px-4 py-2">
+        <table id="table-list" class="display">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Kategori</th>
+                    <th>Deskripsi</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $i = 1;
+                @endphp
+                @foreach ($all_kegiatan as $kegiatan)
+                    <tr>
+                        <td>{{ $i++ }}</td>
+                        <td>{{ $kegiatan->nama_kegiatan }}</td>
+                        <td>{{ Str::title($kegiatan->kategori->nama_kategori) }}</td>
+                        <td>{{ Str::limit($kegiatan->deskripsi, 15, '...') }}</td>
+                        <td>
+                            <a href="/admin/kegiatan/view/{{ $kegiatan->id }}" style="text-decoration: none;"
+                                class="btn text-primary">View</a> |
+                            <form class="d-inline-block" action="{{ route('kegiatan.delete', $kegiatan->id) }}"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button onclick="return confirm('Hapus data ini?')" type="submit"
+                                    class="text-danger btn m-0">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
